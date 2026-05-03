4758e0e6ba8ece7a2f7788b08e1cdf86f06c74cd
Michael Jacobsen <info@michaeljacobsen.ninja>
2026-05-03 10:46:13 +0200

    added update and delete functionality

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -19,7 +19,7 @@ class ChirpController extends Controller
 
 		$chirps = Chirp::with('user')
 			->latest()
-			->simplePaginate(3);
+			->simplePaginate(5);
 
 		// If page query is 1, redirect to home page
 		if ((int) $request->query('page') === 1) {
@@ -72,24 +72,40 @@ class ChirpController extends Controller
 	/**
 	 * Show the form for editing the specified resource.
 	 */
-	public function edit(string $id)
+	public function edit(Chirp $chirp)
 	{
-		//
+		return view('chirps.edit', compact('chirp'));
 	}
 
 	/**
 	 * Update the specified resource in storage.
 	 */
-	public function update(Request $request, string $id)
+	public function update(Request $request, Chirp $chirp)
 	{
-		//
+		// Validate the request
+		$validated = $request->validate([
+			'message' => 'required|string|max:255',
+		], [
+			'message.required' => 'Please write something to chirp!',
+			'message.max' => 'Chirps must be 255 characters or less.',
+		]);
+
+		// Update the chirp
+		$chirp->update($validated);
+
+		// Redirect back to the feed
+		return redirect()->route('home')->with('success', 'Your chirp has been updated!');
 	}
 
 	/**
 	 * Remove the specified resource from storage.
 	 */
-	public function destroy(string $id)
+	public function destroy(Chirp $chirp)
 	{
-		//
+		// Delete the chirp
+		$chirp->delete();
+
+		// Redirect back to the feed
+		return redirect()->route('home')->with('success', 'Your chirp has been deleted!');
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChirpController::class, 'index'])->name('home');
 Route::post('/chirps', [ChirpController::class, 'store'])->name('chirps.create');
+Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
